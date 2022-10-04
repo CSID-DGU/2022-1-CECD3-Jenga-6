35bc18276ee561dddfb37e8d7367a74bb62adc47
YunHo Choi <svs101@dgu.ac.kr>
2022-10-05 03:33:34 +0900

    Feat : VPC 정보 조회 테스트
    - VPC 정보 조회 테스트 코드 작성

---
+++ src/test/java/com/jenga/weather/domain/vpc/VpcTest.java
@@ -0,0 +1,33 @@
+package com.jenga.weather.domain.vpc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeVpcsResponse;
+import software.amazon.awssdk.services.ec2.model.Ec2Exception;
+
+@Slf4j
+public class VpcTest {
+
+    @Test
+    void getDescription() {
+        Region region = Region.AP_NORTHEAST_2;
+        try (Ec2Client ec2 = Ec2Client.builder()
+                .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build()) {
+            DescribeVpcsResponse describeVpcsResponse = ec2.describeVpcs();
+            describeVpcsResponse.vpcs().forEach(vpc -> {
+                log.info("VPC ID : " + vpc.vpcId());
+                log.info("VPC ID's VPC ID : " + vpc.vpcId());
+            });
+        } catch (Ec2Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
