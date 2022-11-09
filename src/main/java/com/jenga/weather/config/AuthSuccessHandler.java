b2777c99068eacf69139ac8c415966df23cc2132
minseong <kdsvip5@naver.com>
2022-11-09 17:54:51 +0900

    Feat : aws-key 저장 여부에 따른 로그인 이후 redirect

--- src/main/java/com/jenga/weather/config/AuthSuccessHandler.java
+++ src/main/java/com/jenga/weather/config/AuthSuccessHandler.java
@@ -1,6 +1,10 @@
 package com.jenga.weather.config;
 
+import com.jenga.weather.domain.member.entity.Member;
+import com.jenga.weather.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +14,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        setDefaultTargetUrl("/aws-key");
-        super.onAuthenticationSuccess(request, response, authentication);
+
+        Member member = memberRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
+
+        if (member.getAccessKey() == null || member.getSecretKey() == null) {
+            setDefaultTargetUrl("/aws-key");
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
+        else {
+            setDefaultTargetUrl("/infra/visualization");
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
     }
 }
