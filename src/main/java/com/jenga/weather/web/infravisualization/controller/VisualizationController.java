e43eda0d92962448a2c07a4dce20eb43df078d8a
minseong <kdsvip5@naver.com>
2022-11-09 18:00:38 +0900

    Feat : html 수정 및 model 값 변경

--- src/main/java/com/jenga/weather/web/infravisualization/controller/VisualizationController.java
+++ src/main/java/com/jenga/weather/web/infravisualization/controller/VisualizationController.java
@@ -22,13 +22,6 @@ public class VisualizationController {
 
     @GetMapping("/infra/visualization")
     public String showInfraVisualization(Model model) {
-        List<VpcDto> vpcList = new ArrayList<>();
-        vpcList.add(new VpcDto("vpc_name_1", "vpc_id_1"));
-        vpcList.add(new VpcDto("vpc_name_2", "vpc_id_2"));
-        vpcList.add(new VpcDto("vpc_name_3", "vpc_id_3"));
-        vpcList.add(new VpcDto("vpc_name_4", "vpc_id_4"));
-
-        model.addAttribute("vpcList", vpcList);
         return "page/infra_visualization";
     }
 
