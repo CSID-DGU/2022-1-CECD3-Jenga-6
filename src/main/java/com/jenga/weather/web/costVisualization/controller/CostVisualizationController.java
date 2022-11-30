e6562cb9892d570879c613b801cfcddeea145cee
Kim Minseong <alstjd98522@gmail.com>
2022-11-30 17:02:04 +0900

    Feat : 리전별 비용 시각화 구현
    - 리전별 비용을 Pie Chart로 시각화
    - 목록을 통해 리전과 상세 비용 출력
    - 비용 차이로 인한 Pie에 보이지 않는 문제 개선 필요

--- src/main/java/com/jenga/weather/web/costVisualization/controller/CostVisualizationController.java
+++ src/main/java/com/jenga/weather/web/costVisualization/controller/CostVisualizationController.java
@@ -1,5 +1,6 @@
 package com.jenga.weather.web.costVisualization.controller;
 
+import com.jenga.weather.web.costVisualization.dto.CostByRegionDto;
 import com.jenga.weather.web.costVisualization.dto.CostByTimeDto;
 import com.jenga.weather.web.costVisualization.service.CostVisualizationService;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +16,31 @@ import java.util.List;
 public class CostVisualizationController {
     private final CostVisualizationService costVisualizationService;
 
-    @GetMapping("/cost/visualization")
-    public String showInfraVisualization(Model model) {
-        List<CostByTimeDto> costList = costVisualizationService.getCostByDate("2022-10-08", "2022-11-09");
+    @GetMapping("/cost/visualization/period")
+    public String showInfraVisualizationByPeriod(Model model) {
+//        List<CostByTimeDto> costList = costVisualizationService.getCostByDate("2022-10-01", "2022-11-01");
 
-        model.addAttribute("costList", costList);
+//        model.addAttribute("vpcCostList", costList);
 
-        return "page/cost_visualization";
+        return "page/cost_visualization_period";
+    }
+
+    @GetMapping("/cost/visualization/region")
+    public String showInfraVisualizationByRegion(Model model) {
+//        List<CostByRegionDto> costList = costVisualizationService.getCostByRegion("2022-10-01", "2022-11-01");
+
+//        model.addAttribute("vpcCostList", costList);
+
+        return "page/cost_visualization_region";
     }
 
     @GetMapping("/cost/result")
     public ResponseEntity<List<CostByTimeDto>> getCost() {
-        return ResponseEntity.ok(costVisualizationService.getCostByDate("2022-10-08", "2022-11-09"));
+        return ResponseEntity.ok(costVisualizationService.getCostByDate("2022-10-01", "2022-11-01"));
+    }
+
+    @GetMapping("/cost/result2")
+    public ResponseEntity<List<CostByRegionDto>> getCost2() {
+        return ResponseEntity.ok(costVisualizationService.getCostByRegion("2022-10-01", "2022-11-01"));
     }
 }
--- src/main/java/com/jenga/weather/web/costVisualization/service/CostVisualizationService.java
+++ src/main/java/com/jenga/weather/web/costVisualization/service/CostVisualizationService.java
@@ -1,7 +1,9 @@
 package com.jenga.weather.web.costVisualization.service;
 
+import com.jenga.weather.web.costVisualization.dto.CostByRegionDto;
 import com.jenga.weather.web.costVisualization.dto.CostByTimeDto;
 import com.jenga.weather.web.costVisualization.dto.CostDto;
+import com.jenga.weather.web.costVisualization.dto.CostDto2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.regions.Region;
@@ -14,6 +16,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CostVisualizationService {
+
     public List<CostByTimeDto> getCostByDate(String startDate, String endDate) {
         GetCostAndUsageRequest costAndUsageRequest = GetCostAndUsageRequest.builder()
                 .timePeriod(DateInterval.builder()
@@ -58,6 +61,53 @@ public class CostVisualizationService {
                             .build());
         }
         return costByTimeDtoList;
+    }
+
+    public List<CostByRegionDto> getCostByRegion(String startDate, String endDate) {
+        GetCostAndUsageRequest costAndUsageRequest = GetCostAndUsageRequest.builder()
+                .timePeriod(DateInterval.builder()
+                        .start(startDate)
+                        .end(endDate)
+                        .build())
+                .granularity("DAILY")
+                .metrics("BLENDED_COST", "USAGE_QUANTITY")
+                .groupBy(GroupDefinition.builder()
+                        .type("DIMENSION")
+                        .key("REGION")
+                        .build())
+                .build();
+        CostExplorerClient costExplorerClient = CostExplorerClient.builder()
+                .region(Region.AWS_GLOBAL)
+                .build();
+
+        List<ResultByTime> resultByRegionList = costExplorerClient.getCostAndUsage(costAndUsageRequest).resultsByTime();
+        List<CostByRegionDto> costByRegionDtoList = new ArrayList<>();
+        double totalCost, totalUtilization;
+        for (ResultByTime result : resultByRegionList) {
+            List<CostDto2> costDtoList = new ArrayList<>();
+            totalCost = 0;
+            totalUtilization = 0;
+            for (Group group : result.groups()) {
+                costDtoList.add(
+                        CostDto2.builder()
+                                .regionName(group.keys().get(0))
+                                .cost(Double.parseDouble(group.metrics().get("BlendedCost").amount()))
+                                .utilization(Double.parseDouble(group.metrics().get("UsageQuantity").amount()))
+                                .build());
+                totalCost += Double.parseDouble(group.metrics().get("BlendedCost").amount());
+                totalUtilization += Double.parseDouble(group.metrics().get("UsageQuantity").amount());
+            }
+
+            costByRegionDtoList.add(
+                    CostByRegionDto.builder()
+                            .date(result.timePeriod().start())
+                            .totalCost(totalCost)
+                            .totalUtilization(totalUtilization)
+                            .costDtoList(costDtoList)
+                            .build());
+        }
+
+        return costByRegionDtoList;
     }
 
     public void setInstanceName(List<CostDto> costDtoList) {
